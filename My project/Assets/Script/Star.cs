5244067852d66da10375ede00f5fc70c8146cd7b
tiGpotS <tolgst12@gmail.com>
2025-06-10 17:46:39 +0900

    250610_C# Star
    별찍기 실습

--- My project/Assets/Script/Star.cs
+++ My project/Assets/Script/Star.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Star : MonoBehaviour
@@ -13,32 +14,28 @@ public class Star : MonoBehaviour
         Phase3();
         Phase4();
         Phase5();
+        Phase6();
     }
+
+    //star += "★"; // 별
+    //star += "　"; // 빈칸
+    //star += "\n"; // 줄바꿈
 
     // 페이즈 1
     public void Phase1()
     {
-        // 1. string.Empty
-        // string 변수의 값을 초기화 합니다.
         star = string.Empty;
 
-        // 2. 별, 빈칸, 줄바꿈 추가하기
-        // Debug.Log를 사용하여 콘솔창에 출력합니다
-        star += "★"; // 별
-        star += "　"; // 빈칸
-        star += "\n"; // 줄바꿈
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 1; i < 6; i++) // 바깥쪽 for문은 세로열의 줄을 의미
         {
-            for(int j = 0; j < i + 1; j++)
+            for (int j = 0; j < i; j++) // 안쪽 for문은 가로줄의 개수를 의미
+                // j(별)은 0부터 시작해서 i가 끝날때 까지(j < 1) 추가된다.
             {
-
+                star += "★"; // 별을 한개씩 추가하겠다.
             }
+            star += "\n"; // 가로줄이 끝나면 줄바꿈을 하겠다.
         }
-        
-
-        // 3. Debug.Log("");
-        // Debug.Log를 사용하여 콘솔창에 출력합니다
         Debug.Log(star);
     }
 
@@ -46,18 +43,43 @@ public class Star : MonoBehaviour
     {
         star = string.Empty;
 
-        // 페이즈 2
+        for (int i = 0; i < 5; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                star += "　";
+            }
 
+            for (int k = 0; k < 5 - i; k++) 
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
 
         Debug.Log(star);
+
     }
 
     public void Phase3()
     {
         star = string.Empty;
 
-        // 페이즈 3
+        for (int i = 5; i >= 1; i--) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                star += "　";
+            }
 
+            for (int k = 0; k < 6 - i; k++) 
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
 
         Debug.Log(star);
     }
@@ -66,9 +88,23 @@ public class Star : MonoBehaviour
     {
         star = string.Empty;
 
-        // 페이즈 4
+        for (int i = 1; i < 6; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                star += "★";
+            }
+            star += "\n";
+        }
 
-
+        for (int i = 4; i >= 1; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                star += "★";
+            }
+            star += "\n";
+        }
         Debug.Log(star);
     }
 
@@ -76,7 +112,72 @@ public class Star : MonoBehaviour
     {
         star = string.Empty;
 
-        // 페이즈 5
+        for (int i = 5; i >= 1; i--) 
+        {
+            for (int j = 1; j < i; j++) 
+            {
+                star += "　";
+            }
+
+            for (int k = 0; k < 6 - i; k++) 
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
+
+        for (int i = 1; i < 5; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                star += "　";
+            }
+
+            for (int k = 0; k < 5 - i; k++) 
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
+
+        Debug.Log(star);
+    }
+
+    public void Phase6()
+    {
+        star = string.Empty;
+
+        for (int i = 1; i < 6; i++) 
+        {
+            for (int j = 5; j > i; j--) 
+            {
+                star += "　";
+            }
+
+            for (int k = 0; k < (2 * i) - 1; k++)
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
+
+        for (int i = 4; i >= 1; i--)
+        {
+            for (int j = 5; j > i; j--) 
+            {
+                star += "　";
+            }
+
+            for (int k = 0; k < (2 * i) - 1; k++) 
+            {
+                star += "★";
+            }
+
+            star += "\n";
+        }
 
 
         Debug.Log(star);
